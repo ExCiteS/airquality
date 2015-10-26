57cb3a68901905c6827d3d81b18e8fcbf9d161f3
Julius O <j.osokinas@mappingforchange.org.uk>
2015-10-26 09:38:21 +0000

    Dev and build files

---
+++ config.sample.js
@@ -0,0 +1,11 @@
+'use strict';
+
+CMAQ.constant('appConfig', {
+  title: 'Community Maps Air Quality',
+  ga: ''
+});
+
+CMAQ.constant('platformConfig', {
+  url: '',
+  client: ''
+});
---
+++ gruntfile.js
@@ -0,0 +1,242 @@
+module.exports = function (grunt) {
+  'use strict';
+
+  require('load-grunt-tasks')(grunt);
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+
+    clean: {
+      build: ['build'],
+      temp: ['temp']
+    },
+
+    copy: {
+      html: {
+        expand: true,
+        dot: true,
+        cwd: 'temp/html',
+        src: '{,*/}*.html',
+        dest: 'build'
+      },
+
+      js: {
+        expand: true,
+        dot: true,
+        cwd: 'temp/js',
+        src: '{,*/}*.js',
+        dest: 'build/scripts'
+      },
+
+      images: {
+        expand: true,
+        dot: true,
+        cwd: 'temp/images',
+        src: '{,*/}*.{png,jpg,jpeg,gif,svg}',
+        dest: 'build/images'
+      }
+    },
+
+    html2js: {
+      app: {
+        src: [
+          'app/templates/partials/**/*.html',
+        ],
+        dest: 'temp/js/templates.js'
+      },
+
+      options: {
+        base: 'app/templates',
+        module: 'templates',
+        htmlmin: {
+          conservativeCollapse: true,
+          collapseWhitespace: true,
+          removeComments: true,
+          removeOptionalTags: true
+        }
+      }
+    },
+
+    htmlmin: {
+      index: {
+        files: [{
+          expand: true,
+          flatten: true,
+          cwd: 'temp',
+          src: 'html/index.html',
+          dest: 'temp/html'
+        }]
+      },
+
+      options: {
+        conservativeCollapse: true,
+        collapseWhitespace: true,
+        removeComments: true,
+        removeOptionalTags: true
+      }
+    },
+
+    imagemin: {
+      app: {
+        files: [{
+          expand: true,
+          cwd: 'app/images',
+          src: '{,*/}*.{png,jpg,jpeg,gif,svg}',
+          dest: 'temp/images'
+        }]
+      }
+    },
+
+    concat: {
+      vendor: {
+        src: [
+          'bower_components/lodash/lodash.js',
+          'bower_components/leaflet/dist/leaflet.js',
+          'bower_components/moment/moment.js',
+          'bower_components/angular/angular.js',
+          'bower_components/angular-sanitize/angular-sanitize.js',
+          'bower_components/angular-moment/angular-moment.js',
+          'bower_components/angular-ui-router/release/angular-ui-router.js'
+        ],
+        dest: 'temp/js/vendor.js'
+      },
+
+      app: {
+        src: [
+          'app/scripts/AppInit.js',
+          'app/scripts/{,*/}*.js',
+          'config.js'
+        ],
+        dest: 'temp/js/app.js'
+      },
+
+      options: {
+        stripBanners: true
+      }
+    },
+
+    uglify: {
+      vendor: {
+        files: {
+          'temp/js/vendor.js': ['temp/js/vendor.js']
+        }
+      },
+
+      app: {
+        files: {
+          'temp/js/app.js': ['temp/js/app.js']
+        }
+      }
+    },
+
+    ngAnnotate: {
+      app: {
+        files: [{
+          'temp/js/app.js': ['temp/js/app.js']
+        }]
+      },
+
+      options: {
+        singleQuotes: true
+      }
+    },
+
+    replace: {
+      app: {
+        files: [{
+            src: ['app/templates/index.html'],
+            dest: 'temp/html/index.html'
+          },
+
+          {
+            src: ['temp/js/app.js'],
+            dest: 'temp/js/app.js'
+          }
+        ]
+      },
+
+      options: {
+        patterns: [{
+          match: 'version',
+          replacement: '<%= pkg.version %>'
+        }]
+      }
+    },
+
+    jshint: {
+      app: ['config.js', 'app/scripts/**/*.js'],
+
+      options: {
+        jshintrc: '.jshintrc'
+      }
+    },
+
+    watch: {
+      templates: {
+        files: ['app/templates/**/*.html'],
+        tasks: [
+          'html2js',
+          'replace',
+          'copy:html',
+          'copy:js'
+        ]
+      },
+
+      images: {
+        files: ['app/images/{,*/}*.{png,jpg,jpeg,gif}'],
+        tasks: [
+          'imagemin',
+          'copy:images'
+        ]
+      },
+
+      scripts: {
+        files: ['config.js', 'app/scripts/{,*/}*.js'],
+        tasks: [
+          'concat:app',
+          'copy:js'
+        ]
+      },
+
+      options: {
+        spawn: false
+      }
+    }
+  });
+
+  grunt.registerTask('dev', [
+    'jshint',
+    'clean:temp',
+    'html2js',
+    'imagemin',
+    'concat',
+    'replace',
+    'clean:build',
+    'copy:html',
+    'copy:js',
+    'copy:images',
+    'clean:temp',
+    'watch'
+  ]);
+
+  grunt.registerTask('build', [
+    'jshint',
+    'clean:temp',
+    'html2js',
+    'imagemin',
+    'concat',
+    'replace',
+    'htmlmin',
+    'ngAnnotate',
+    'uglify',
+    'clean:build',
+    'copy:html',
+    'copy:js',
+    'copy:images',
+    'clean:temp'
+  ]);
+
+  grunt.registerTask('default', function () {
+    grunt.log.writeln('Please use `grunt dev` or `grunt build` instead.');
+  });
+};
