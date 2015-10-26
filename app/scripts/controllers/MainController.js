4ffd096fe829a743e537aa01810d8f3742d7566c
Julius O <j.osokinas@mappingforchange.org.uk>
2015-10-26 11:58:17 +0000

    Not exposing all platform configuration, only URL

--- app/scripts/controllers/MainController.js
+++ app/scripts/controllers/MainController.js
@@ -1,7 +1,8 @@
 'use strict';
 
 CMAQ.controller('MainController', function ($scope, platformConfig, data, viewport, state) {
-  $scope.platformConfig = platformConfig;
+  viewport.platform = platformConfig.url;
+
   $scope.data = data;
   $scope.viewport = viewport;
   $scope.state = state;
