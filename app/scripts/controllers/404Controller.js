253ce7c0f4131c530822adfbf19e6bba7b814316
Julius O <j.osokinas@mappingforchange.org.uk>
2015-10-27 10:32:26 +0000

    404 state now displays a common message

--- app/scripts/controllers/404Controller.js
+++ app/scripts/controllers/404Controller.js
@@ -1,5 +1,6 @@
 'use strict';
 
-CMAQ.controller('404Controller', function (state) {
-  state.setTitle('404');
+CMAQ.controller('404Controller', function (viewport, state) {
+  viewport.message = 'It looks like something went wrong. Shall we try again?';
+  state.redirect();
 });
