c397c6fa7111b60a9bfb8b4ea2c4213e21e61306
Julius O <j.osokinas@mappingforchange.org.uk>
2015-10-27 16:47:18 +0000

    Message when points is not found

--- app/scripts/controllers/PointController.js
+++ app/scripts/controllers/PointController.js
@@ -1,5 +1,23 @@
 'use strict';
 
-CMAQ.controller('PointController', function ($scope, state) {
+CMAQ.controller('PointController', function ($stateParams, $scope, data, viewport, state) {
+  var pointId = $stateParams.pointId;
+
   state.setTitle('Point');
+  delete data.point;
+
+  if (_.isEmpty(data.points)) {
+
+  } else {
+    var point = _.find(data.points, function (currentPoint) {
+      return currentPoint.id === pointId;
+    });
+
+    if (point) {
+      data.point = point;
+    } else {
+      viewport.message = 'It looks like the data point can\'t be found. Please choose an existing one.';
+      state.redirect();
+    }
+  }
 });
