e497459c13055bc76b40bb91382fa83403da6feb
Julius O <j.osokinas@mappingforchange.org.uk>
2016-01-12 17:39:52 +0000

    Removed testing on Opera
    Such a pain…

--- karma.config.js
+++ karma.config.js
@@ -12,7 +12,6 @@ module.exports = function (karma) {
       'karma-phantomjs-launcher',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
-      'karma-opera-launcher',
       'karma-coverage'
     ],
 
