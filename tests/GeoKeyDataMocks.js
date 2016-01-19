0275c33c5bf267617b1307a724db67c1ace712a4
Julius O <j.osokinas@mappingforchange.org.uk>
2016-01-19 16:53:44 +0000

    Additional tests for api factory

--- tests/GeoKeyDataMocks.js
+++ tests/GeoKeyDataMocks.js
@@ -9,7 +9,42 @@ var authenticationMock = {
 };
 
 /**
+ * LOCATION
+ */
+
+var locationMock = {
+  id: 1,
+  type: 'Feature',
+  geometry: {},
+  name: 'First location',
+  created: '2015-09-15T09:40:01.747Z',
+  properties: {
+    height: 2,
+    distance: 3.5,
+    characteristics: null
+  },
+  measurements: []
+};
+
+/**
  * LOCATIONS
  */
 
 var locationsMock = [];
+
+locationsMock.push(locationMock);
+
+/**
+ * PROJECTS
+ */
+
+var projectsMock = [{
+    id: 1,
+    name: 'First project'
+  },
+
+  {
+    id: 2,
+    name: 'Second project'
+  }
+];
--- tests/factories/apiFactory.js
+++ tests/factories/apiFactory.js
@@ -1,7 +1,7 @@
 describe('Factory: api', function () {
   'use strict';
 
-  var q, httpBackend;
+  var q, httpBackend, rootScope;
   var apiFactory, dataFactory, viewportFactory, oauthFactory;
   var store = {},
     callbacks;
@@ -25,9 +25,10 @@ describe('Factory: api', function () {
     callbacks = function () {};
   });
 
-  beforeEach(inject(function ($q, $httpBackend, api, data, viewport, oauth) {
+  beforeEach(inject(function ($q, $httpBackend, $rootScope, api, data, viewport, oauth) {
     q = $q;
     httpBackend = $httpBackend;
+    rootScope = $rootScope;
     apiFactory = api;
     dataFactory = data;
     viewportFactory = viewport;
@@ -164,6 +165,8 @@ describe('Factory: api', function () {
       apiFactory.getUserInfo()
         .then(callbacks.successCallback)
         .catch(callbacks.errorCallback);
+
+      rootScope.$digest();
     });
 
     it('should reject on error', function () {
@@ -259,6 +262,8 @@ describe('Factory: api', function () {
           .then(callbacks.successCallback)
           .catch(callbacks.errorCallback);
 
+        rootScope.$digest();
+
         expect(dataFactory.locations).toEqual(null);
       });
 
@@ -285,6 +290,8 @@ describe('Factory: api', function () {
         apiFactory.getLocations()
           .then(callbacks.successCallback)
           .catch(callbacks.errorCallback);
+
+        rootScope.$digest();
 
         expect(dataFactory.locations).toEqual(locationsMock);
       });
@@ -347,6 +354,365 @@ describe('Factory: api', function () {
         httpBackend.flush();
 
         expect(dataFactory.locations).toEqual(locationsMock);
+      });
+    });
+  }
+
+  describe('Public method: addLocation', function () {
+    var location;
+
+    beforeEach(function () {
+      var deferred = q.defer();
+
+      spyOn(apiFactory, 'sync').and.callFake(function () {
+        deferred.resolve();
+        return deferred.promise;
+      });
+      spyOn(oauthFactory, 'refresh').and.callFake(function () {
+        deferred.resolve();
+        return deferred.promise;
+      });
+
+      dataFactory.locations = [];
+
+      location = {
+        type: 'Feature',
+        geometry: {},
+        name: 'First location',
+        properties: {
+          height: 2,
+          distance: 3.5,
+          characteristics: null
+        }
+      };
+    });
+
+    it('should throw an error when location is not specified', function () {
+      expect(function () {
+        apiFactory.addLocation();
+      }).toThrow(new Error('Location not specified'));
+    });
+
+    it('should throw an error when location is not plain object', function () {
+      _.each(excludingPlainObjectMock, function (location) {
+        expect(function () {
+          apiFactory.addLocation(location);
+        }).toThrow(new Error('Location must be plain object'));
+      });
+    });
+
+    it('should resolve when offline and there are no local locations added yet', function () {
+      var deferred = q.defer();
+
+      spyOn(apiFactory, 'online').and.callFake(function () {
+        deferred.reject();
+        return deferred.promise;
+      });
+
+      callbacks.successCallback = function () {
+        expect(callbacks.successCallback).toHaveBeenCalled();
+      };
+      callbacks.errorCallback = function () {
+        expect(callbacks.errorCallback).not.toHaveBeenCalled();
+      };
+
+      spyOn(callbacks, 'successCallback').and.callThrough();
+      spyOn(callbacks, 'errorCallback').and.callThrough();
+
+      apiFactory.addLocation(location)
+        .then(callbacks.successCallback)
+        .catch(callbacks.errorCallback);
+
+      rootScope.$digest();
+
+      expect(dataFactory.locations.length).toEqual(1);
+      expect(dataFactory.locations[0].id).toEqual('x1');
+    });
+
+    it('should resolve when offline and there are already local locations added', function () {
+      var deferred = q.defer();
+
+      dataFactory.locations.push({
+        id: 'x1'
+      });
+      dataFactory.unsynced.locations.push(dataFactory.locations[0]);
+
+      spyOn(apiFactory, 'online').and.callFake(function () {
+        deferred.reject();
+        return deferred.promise;
+      });
+
+      callbacks.successCallback = function () {
+        expect(callbacks.successCallback).toHaveBeenCalled();
+      };
+      callbacks.errorCallback = function () {
+        expect(callbacks.errorCallback).not.toHaveBeenCalled();
+      };
+
+      spyOn(callbacks, 'successCallback').and.callThrough();
+      spyOn(callbacks, 'errorCallback').and.callThrough();
+
+      apiFactory.addLocation(location)
+        .then(callbacks.successCallback)
+        .catch(callbacks.errorCallback);
+
+      rootScope.$digest();
+
+      expect(dataFactory.locations.length).toEqual(2);
+      expect(dataFactory.locations[0].id).toEqual('x1');
+      expect(dataFactory.locations[1].id).toEqual('x2');
+    });
+
+    it('should reject on error', function () {
+      var deferred = q.defer();
+
+      spyOn(apiFactory, 'online').and.callFake(function () {
+        deferred.resolve();
+        return deferred.promise;
+      });
+
+      callbacks.successCallback = function () {
+        expect(callbacks.successCallback).not.toHaveBeenCalled();
+      };
+      callbacks.errorCallback = function () {
+        expect(callbacks.errorCallback).toHaveBeenCalled();
+      };
+
+      spyOn(callbacks, 'successCallback').and.callThrough();
+      spyOn(callbacks, 'errorCallback').and.callThrough();
+
+      apiFactory.addLocation(location)
+        .then(callbacks.successCallback)
+        .catch(callbacks.errorCallback);
+
+      httpBackend.when('POST', /\.*airquality\/locations.*/).respond(500);
+      httpBackend.flush();
+
+      expect(dataFactory.locations.length).toEqual(0);
+    });
+
+    it('should resolve on success when adding location not from local one', function () {
+      var deferred = q.defer();
+
+      spyOn(apiFactory, 'online').and.callFake(function () {
+        deferred.resolve();
+        return deferred.promise;
+      });
+
+      callbacks.successCallback = function () {
+        expect(callbacks.successCallback).toHaveBeenCalled();
+      };
+      callbacks.errorCallback = function () {
+        expect(callbacks.errorCallback).not.toHaveBeenCalled();
+      };
+
+      spyOn(callbacks, 'successCallback').and.callThrough();
+      spyOn(callbacks, 'errorCallback').and.callThrough();
+
+      apiFactory.addLocation(location)
+        .then(callbacks.successCallback)
+        .catch(callbacks.errorCallback);
+
+      httpBackend.when('POST', /\.*airquality\/locations.*/).respond(200, locationMock);
+      httpBackend.flush();
+
+      expect(dataFactory.locations.length).toEqual(1);
+    });
+
+    it('should resolve on success when adding location from local one', function () {
+      var deferred = q.defer();
+
+      location.id = 'x1';
+      dataFactory.locations.push(location);
+
+      spyOn(apiFactory, 'online').and.callFake(function () {
+        deferred.resolve();
+        return deferred.promise;
+      });
+
+      callbacks.successCallback = function () {
+        expect(callbacks.successCallback).toHaveBeenCalled();
+      };
+      callbacks.errorCallback = function () {
+        expect(callbacks.errorCallback).not.toHaveBeenCalled();
+      };
+
+      spyOn(callbacks, 'successCallback').and.callThrough();
+      spyOn(callbacks, 'errorCallback').and.callThrough();
+
+      apiFactory.addLocation(location)
+        .then(callbacks.successCallback)
+        .catch(callbacks.errorCallback);
+
+      httpBackend.when('POST', /\.*airquality\/locations.*/).respond(200, locationMock);
+      httpBackend.flush();
+
+      expect(dataFactory.locations.length).toEqual(1);
+    });
+  });
+
+  // Exclude Firefox
+  if (navigator.userAgent.indexOf('Firefox') == -1) {
+    describe('Public method: getProjects', function () {
+      beforeEach(function () {
+        var deferred = q.defer();
+
+        spyOn(apiFactory, 'sync').and.callFake(function () {
+          deferred.resolve();
+          return deferred.promise;
+        });
+        spyOn(oauthFactory, 'refresh').and.callFake(function () {
+          deferred.resolve();
+          return deferred.promise;
+        });
+      });
+
+      it('should resolve when offline and there are no projects in storage', function () {
+        var deferred = q.defer();
+
+        store.PROJECTS = undefined;
+
+        spyOn(apiFactory, 'online').and.callFake(function () {
+          deferred.reject();
+          return deferred.promise;
+        });
+
+        callbacks.successCallback = function () {
+          expect(callbacks.successCallback).toHaveBeenCalled();
+        };
+        callbacks.errorCallback = function () {
+          expect(callbacks.errorCallback).not.toHaveBeenCalled();
+        };
+
+        spyOn(callbacks, 'successCallback').and.callThrough();
+        spyOn(callbacks, 'errorCallback').and.callThrough();
+
+        apiFactory.getProjects()
+          .then(callbacks.successCallback)
+          .catch(callbacks.errorCallback);
+
+        rootScope.$digest();
+
+        expect(dataFactory.projects).toEqual([]);
+      });
+
+      it('should resolve when offline and there are projects in storage', function () {
+        var deferred = q.defer();
+
+        store.PROJECTS = JSON.stringify(projectsMock);
+
+        spyOn(apiFactory, 'online').and.callFake(function () {
+          deferred.reject();
+          return deferred.promise;
+        });
+
+        callbacks.successCallback = function () {
+          expect(callbacks.successCallback).toHaveBeenCalled();
+        };
+        callbacks.errorCallback = function () {
+          expect(callbacks.errorCallback).not.toHaveBeenCalled();
+        };
+
+        spyOn(callbacks, 'successCallback').and.callThrough();
+        spyOn(callbacks, 'errorCallback').and.callThrough();
+
+        apiFactory.getProjects()
+          .then(callbacks.successCallback)
+          .catch(callbacks.errorCallback);
+
+        rootScope.$digest();
+
+        expect(dataFactory.projects).toEqual(projectsMock);
+      });
+
+      it('should reject on error and there are no projects in storage', function () {
+        var deferred = q.defer();
+
+        store.PROJECTS = undefined;
+
+        spyOn(apiFactory, 'online').and.callFake(function () {
+          deferred.resolve();
+          return deferred.promise;
+        });
+
+        callbacks.successCallback = function () {
+          expect(callbacks.successCallback).not.toHaveBeenCalled();
+        };
+        callbacks.errorCallback = function () {
+          expect(callbacks.errorCallback).toHaveBeenCalled();
+        };
+
+        spyOn(callbacks, 'successCallback').and.callThrough();
+        spyOn(callbacks, 'errorCallback').and.callThrough();
+
+        apiFactory.getProjects()
+          .then(callbacks.successCallback)
+          .catch(callbacks.errorCallback);
+
+        httpBackend.when('GET', /\.*airquality\/projects.*/).respond(500);
+        httpBackend.flush();
+
+        expect(dataFactory.projects).toEqual([]);
+      });
+
+      it('should reject on error and there are projects in storage', function () {
+        var deferred = q.defer();
+
+        store.PROJECTS = JSON.stringify(projectsMock);
+
+        spyOn(apiFactory, 'online').and.callFake(function () {
+          deferred.resolve();
+          return deferred.promise;
+        });
+
+        callbacks.successCallback = function () {
+          expect(callbacks.successCallback).not.toHaveBeenCalled();
+        };
+        callbacks.errorCallback = function () {
+          expect(callbacks.errorCallback).toHaveBeenCalled();
+        };
+
+        spyOn(callbacks, 'successCallback').and.callThrough();
+        spyOn(callbacks, 'errorCallback').and.callThrough();
+
+        apiFactory.getProjects()
+          .then(callbacks.successCallback)
+          .catch(callbacks.errorCallback);
+
+        httpBackend.when('GET', /\.*airquality\/projects.*/).respond(500);
+        httpBackend.flush();
+
+        expect(dataFactory.projects).toEqual(projectsMock);
+      });
+
+      it('should resolve on success', function () {
+        var deferred = q.defer();
+
+        store.PROJECTS = undefined;
+
+        spyOn(apiFactory, 'online').and.callFake(function () {
+          deferred.resolve();
+          return deferred.promise;
+        });
+
+        callbacks.successCallback = function () {
+          expect(callbacks.successCallback).toHaveBeenCalled();
+        };
+        callbacks.errorCallback = function () {
+          expect(callbacks.errorCallback).not.toHaveBeenCalled();
+        };
+
+        spyOn(callbacks, 'successCallback').and.callThrough();
+        spyOn(callbacks, 'errorCallback').and.callThrough();
+
+        apiFactory.getProjects()
+          .then(callbacks.successCallback)
+          .catch(callbacks.errorCallback);
+
+        httpBackend.when('GET', /\.*airquality\/projects.*/).respond(200, projectsMock);
+        httpBackend.flush();
+
+        expect(dataFactory.projects).toEqual(projectsMock);
       });
     });
   }
