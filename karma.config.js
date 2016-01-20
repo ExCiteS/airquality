5ee8e1dd23d17026b64cbad85f1652042a9633d0
Julius O <j.osokinas@mappingforchange.org.uk>
2016-01-20 11:25:04 +0000

    Not testing on IE

--- karma.config.js
+++ karma.config.js
@@ -12,7 +12,6 @@ module.exports = function (karma) {
       'karma-phantomjs-launcher',
       'karma-chrome-launcher',
       'karma-firefox-launcher',
-      'karma-ie-launcher',
       'karma-coverage'
     ],
 
